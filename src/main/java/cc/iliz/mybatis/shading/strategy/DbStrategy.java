01f9db07b90ce8b89988447ee505a8fd50b0d9c5
lizhengjava <lizhengjava@126.com>
2018-09-03 21:09:10 +0800

    update java doc & readme

--- src/main/java/cc/iliz/mybatis/shading/strategy/DbStrategy.java
+++ src/main/java/cc/iliz/mybatis/shading/strategy/DbStrategy.java
@@ -2,7 +2,17 @@ package cc.iliz.mybatis.shading.strategy;
 
 import cc.iliz.mybatis.shading.db.ShardingEntry;
 
+/**
+ * 数据库分库策略，实现后用{@code Component}组件注册到spring中
+ * @author lizhengjava
+ *
+ */
 public interface DbStrategy {
 
+	/**
+	 * 自定义数据库分表策略
+	 * @param shardingEntry 分表策略参数
+	 * @return 数据源spring配置名称
+	 */
 	public String getShardingDataSource(ShardingEntry shardingEntry);
 }
